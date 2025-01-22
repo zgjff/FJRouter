eacffaaceda22a123fd5ae34492f906627d3912e
清风垂露 <zguijie1005@163.com>
2025-01-22 13:52:03 +0800

    add EmitEventByName 方便协议方法的默认值

--- Sources/Event/FJRouter+Event.swift
+++ Sources/Event/FJRouter+Event.swift
@@ -26,15 +26,12 @@ public protocol FJRouterEventable {
     /// - Parameters:
     ///   - location: 路径.
     ///   - extra: 携带的参数
-    func emit(_ location: String, extra: (any Sendable)?) async throws
+    func emit(_ location: String, extra: @autoclosure @escaping @Sendable () -> (any Sendable)?) throws
     
     /// 通过事件名称触发事件
     /// - Parameters:
-    ///   - name: 事件名称
-    ///   - params: 路由参数
-    ///   - queryParams: 路由查询参数
-    ///   - extra: 携带的参数
-    func emit(byName name: String, params: [String : String], queryParams: [String : String], extra: (any Sendable)?) async throws
+    ///   - parameters: 事件名称参数
+    func emit(byName parameters: FJRouter.EmitEventByName) throws
 }
 
 extension FJRouterEventable {
@@ -48,31 +45,37 @@ extension FJRouterEventable {
     /// 通过事件url路径触发事件
     /// - Parameters:
     ///   - location: 路径
-    public func emit(_ location: String) async throws {
-        try await emit(location, extra: nil)
+    public func emit(_ location: String) throws {
+        try emit(location, extra: nil)
     }
     
     /// 通过事件名称触发事件
     /// - Parameters:
     ///   - name: 事件名称
-    public func emit(byName name: String) async throws {
-        try await emit(byName: name, params: [:], queryParams: [:], extra: nil)
+    public func emit(byName name: String) throws {
+        try emit(byName: .init(name: name))
     }
-    
-    /// 通过事件名称触发事件
-    /// - Parameters:
-    ///   - name: 事件名称
-    ///   - params: 路由参数
-    public func emit(byName name: String, params: [String : String]) async throws {
-        try await emit(byName: name, params: params, queryParams: [:], extra: nil)
-    }
-    
-    /// 通过事件名称触发事件
-    /// - Parameters:
-    ///   - name: 事件名称
-    ///   - params: 路由参数
-    ///   - queryParams: 路由查询参数
-    public func emit(byName name: String, params: [String : String], queryParams: [String : String]) async throws {
-        try await emit(byName: name, params: params, queryParams: queryParams, extra: nil)
+}
+
+extension FJRouter {
+    /// 通过事件名称触发事件参数: 为了方便协议方法的默认值
+    public struct EmitEventByName {
+        internal let name: String
+        internal let params: [String : String]
+        internal let queryParams: [String : String]
+        internal let extra: @Sendable () -> (any Sendable)?
+        
+        /// 通过事件名称触发事件参数初始化
+        /// - Parameters:
+        ///   - name: 事件名称
+        ///   - params: 路由参数
+        ///   - queryParams: 路由查询参数
+        ///   - extra: 携带的参数
+        public init(name: String, params: [String : String] = [:], queryParams: [String : String] = [:], extra: @autoclosure @escaping @Sendable () -> (any Sendable)? = nil) {
+            self.name = name
+            self.params = params
+            self.queryParams = queryParams
+            self.extra = extra
+        }
     }
 }
--- Sources/Event/FJRouterEventImpl.swift
+++ Sources/Event/FJRouterEventImpl.swift
@@ -21,23 +21,31 @@ extension FJRouter {
 
 extension FJRouter.EventImpl: FJRouterEventable {
     func onReceive(path: String, name: String?) async throws -> AnyPublisher<FJRouter.EventMatchInfo, Never> {
-        let action = try FJRouterEventAction(path: path, name: nil)
+        let action = try FJRouterEventAction(path: path, name: name)
         let listener = await store.saveOrCreateListener(action: action)
         return listener.publisher()
     }
 
-    func emit(_ location: String, extra: (any Sendable)? = nil) async throws {
+    func emit(_ location: String, extra: @autoclosure @escaping @Sendable () -> (any Sendable)?) throws {
         guard let url = URL(string: location) else {
             return
         }
-        guard let (listener, info) = await store.match(url: url, extra: extra) else {
-            return
+        Task {
+            guard let (listener, info) = await self.store.match(url: url, extra: extra()) else {
+                return
+            }
+            listener.receive(value: info)
         }
-        listener.receive(value: info)
     }
     
-    func emit(byName name: String, params: [String : String], queryParams: [String : String], extra: (any Sendable)?) async throws {
-        let loc =  try await store.convertLocationBy(name: name, params: params, queryParams: queryParams)
-        try await emit(loc, extra: extra)
+    func emit(byName parameters: FJRouter.EmitEventByName) throws {
+        try emit(byName: parameters.name, params: parameters.params, queryParams: parameters.queryParams, extra: parameters.extra)
+    }
+    
+    func emit(byName name: String, params: [String : String], queryParams: [String : String], extra: @autoclosure @escaping @Sendable () -> (any Sendable)?) throws {
+        Task {
+            let loc =  try await store.convertLocationBy(name: name, params: params, queryParams: queryParams)
+            try self.emit(loc, extra: extra)
+        }
     }
 }
