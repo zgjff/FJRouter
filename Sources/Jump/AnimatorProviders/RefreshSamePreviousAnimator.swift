b428d97698579e51e1fa8dc48da79b52b15f1393
zgjff <zguijie1005@163.com>
2025-04-12 01:44:02 +0800

    update  RefreshSamePreviousAnimator

--- Sources/Jump/AnimatorProviders/RefreshSamePreviousAnimator.swift
+++ Sources/Jump/AnimatorProviders/RefreshSamePreviousAnimator.swift
@@ -15,8 +15,8 @@ extension FJRoute {
     /// 一般用于, 当匹配到的路由指向控制器, 与app最上层正在展示的控制器是同一类, 按照设计要求,
     /// 此时不需要跳转新的页面, 而是需要更新正在展示的控制器内容. eg:
     ///
-    ///     try await FJRouter.shared.registerRoute(FJRoute(path: "/four", name: "four", builder: { sourceController, state in
-    ///         FourViewController()
+    ///     await FJRouter.jump().registerRoute(try FJRoute(path: "/third", name: "thrid", builder: { info in
+    ///         ThirdViewController()
     ///     }, animator: { info in
     ///         if let pvc = info.fromVC, pvc is FourViewController { // 或者其它判断条件
     ///             return FJRoute.RefreshSamePreviousAnimator { @Sendable previousVC, state in
@@ -35,7 +35,10 @@ extension FJRoute {
         }
         
         public func startAnimated(from fromVC: UIViewController?, to toVC: @escaping @MainActor () -> UIViewController, state matchState: FJRouterState) {
-            
+            guard let fromVC else {
+                return
+            }
+            refresh(fromVC, matchState)
         }
     }
 }
