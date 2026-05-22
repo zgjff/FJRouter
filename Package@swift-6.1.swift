76c99f814a42a3722be297f1086641ea933aefdf
清风垂露 <zguijie1005@163.com>
2026-05-22 13:38:50 +0800

    update file

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -2,51 +2,23 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import CompilerPluginSupport
 
 let package = Package(
     name: "FJRouter",
     platforms: [
-        .macOS(.v10_15), // why: support test macros, macros only can test for mac
         .iOS(.v13),
-        .macCatalyst(.v13),
     ],
     products: [
         .library(name: "FJRouter", targets: ["FJRouter"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.1"),
     ],
     targets: [
         .target(name: "FJRouter",
             resources: [.process("PrivacyInfo.xcprivacy")]
         ),
-        
         .testTarget(
-            name: "RouterTest",
+            name: "RouterTests",
             dependencies: [
                 "FJRouter",
-            ],
-        ),
-        
-        .macro(
-            name: "FJRouterMacroPlugins",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ]
-        ),
-        
-        .target(name: "FJRouterMacro",
-            dependencies: ["FJRouterMacroPlugins"]
-        ),
-        
-        .testTarget(
-            name: "MacroTest",
-            dependencies: [
-                "FJRouterMacro",
-                "FJRouterMacroPlugins",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
             ],
         ),
     ],
